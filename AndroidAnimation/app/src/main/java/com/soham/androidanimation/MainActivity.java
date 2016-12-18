f65ab10e78484eca5e6a3d6e7cef64f9091f3c76
sohamnavadiya <soham@auberginesolutions.com>
2016-12-18 13:58:22 +0530

    Adding simple transition in text on button click.

--- AndroidAnimation/app/src/main/java/com/soham/androidanimation/MainActivity.java
+++ AndroidAnimation/app/src/main/java/com/soham/androidanimation/MainActivity.java
@@ -1,7 +1,13 @@
 package com.soham.androidanimation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout view = (LinearLayout) findViewById(R.id.transitions_container);
+        final ViewGroup transitionsContainer = (ViewGroup) view.findViewById(R.id.transitions_container);
+        final TextView text = (TextView) view.findViewById(R.id.text);
+        final Button button = (Button) view.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            boolean visible;
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(transitionsContainer);
+                visible = !visible;
+                text.setVisibility(visible ? View.VISIBLE : View.GONE);
+            }
+        });
     }
 }
